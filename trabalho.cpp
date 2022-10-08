7bad8a388faca7c6c4cbd7ce9a9da96f94ca8012
RickelmeFarias <rickelmekaua7@gmail.com>
2022-10-08 10:53:42 -0300

    adicionando cardapio

--- trabalho.cpp
+++ trabalho.cpp
@@ -25,3 +25,32 @@ int main(){
 	printf("Escolha uma opcao: ");
 	scanf("%d", &options);
 }
+int cardapio(){
+	int type;
+	printf("|======== | Cardapio | ========|\n");
+	printf("|        1 - Entradas          |\n");
+	printf("|        2 - Bebidas           |\n");
+	printf("|==============================|\n");
+	printf("|       Faca seu pedido        |\n");
+	printf("|==============================|\n");
+	scanf("%d", &type);
+	switch(type){
+		case 1: {
+			printf("|=========== | Entradas | ===========|");
+			printf("| 1 - Hamburguer da casa    R$ 13,00 |");
+			printf("| 2 - pastel carne/queijo   R$ 5,00  |");
+			printf("| 3 - bomba presunto/queijo R$ 6,50  |");
+			printf("| 4 - coxinha de frango     R$ 6,00  |");
+			break;
+		}
+		case 2: {
+			printf("|============ | Bebida | ============|");
+			printf("| 1 - Suco 350ml R$ 6,00             |");
+			printf("| 2 - Refrigerante lata R$ 5,00      |");
+            printf("| 3 - Suco Litro R$ 10,00            |");
+			printf("| 4 - Refrigerante Litro R$ 12,00    |");
+			printf("|====================================|");	
+			break;
+		}
+	}
+}
